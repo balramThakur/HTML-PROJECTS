6c0abbe7d3c08d3945259e7f855a64f415c2b6de
balramThakur <99987850+balramThakur@users.noreply.github.com>
2023-01-07 19:07:23 +0530

    .

---
+++ home.js
@@ -0,0 +1,15 @@
+
+function inc(){
+    
+      var x = document.getElementById("ch");
+      
+      if (x.style.display == "flow-root"){
+      
+             x.style.display = "none";
+      
+      }
+      else {
+            x.style.display = "flow-root";
+      }
+      
+      }
